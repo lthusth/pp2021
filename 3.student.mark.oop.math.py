66d24cbc15894b7dad3a75ee18f00012b228e6af
lthusth <hunglt.bi10-074@st.usth.edu.vn>
2021-05-01 17:23:48 +0700

    fix code labwork3 and add new curse UI

--- 3.student.mark.oop.math.py
+++ 3.student.mark.oop.math.py
@@ -1,3 +1,5 @@
+import time
+
 student_list = []
 course_list = []
 mark_list = []
@@ -6,11 +8,15 @@ import curses
 import math
 
 screen = curses.initscr()
-screen.addstr("Initializing screen...\n"
-              "Screen initialized.")
-screen.refresh()
-curses.napms(2000)
-screen.clear()
+
+
+# screen.addstr("Initializing screen...\n")
+# screen.refresh()
+# curses.napms(1000)
+# screen.addstr("Screen initialized.\n")
+# screen.refresh()
+# screen.clear()
+# curses.napms(2000)
 
 
 class Student:
@@ -36,6 +42,7 @@ class Student:
         self._gpa = gpa
 
     def input_sinfo(self):
+        screen.clear()
         screen.addstr("Enter student ID: ")
         self._sid = screen.getstr().decode()
         screen.addstr("Enter student name: ")
@@ -46,7 +53,7 @@ class Student:
         screen.refresh()
 
     def __str__(self):
-        return f" Student name is: {self._sname}, with ID: {self._sid} , dob is: {self._dob}"
+        return f" Student name is: {self._sname}, with ID: {self._sid}, dob is: {self._dob} \n"
 
     def show_student(self):
         screen.addstr(self.__str__())
@@ -76,6 +83,7 @@ class Course:
         return int(self._credits)
 
     def input_cinfo(self):
+        screen.clear()
         screen.addstr("Enter course ID: ")
         self._cid = screen.getstr().decode()
         screen.addstr("Enter course name: ")
@@ -85,9 +93,7 @@ class Course:
         screen.clear()
 
     def __str__(self):
-        return f""" Course name is: {self._cname},
-        with course ID: {self._cid},
-        {self._credits} credits """
+        return f" Course name is: {self._cname}, with course ID: {self._cid}, {self._credits} credits \n"
 
     def show_course(self):
         screen.addstr(self.__str__())
@@ -114,30 +120,29 @@ class Mark:
         return self._course
 
     def getmark(self):
-        return int(self._value)
+        return float(self._value)
 
     def inputmark(self):
         screen.addstr(f"Enter mark for student {self._student.getsname()}:")
-        self._value = int(screen.getstr().decode())
-        screen.clear()
+        self._value = float(screen.getstr().decode())
 
     def __str__(self):
-        return f""" {self._student.getsname()} 
-                got {self._value} 
-                in course: {self._course.getcname()}"""
+        return f" {self._student.getsname()} got {self._value} in course {self._course.getcname()} \n"
 
     def show_mark(self):
         screen.addstr(self.__str__())
 
 
 screen.addstr("__________Input mark for each course____________ \n")
-
 for j in range(len(course_list)):
     screen.addstr("for course {} \n".format(course_list[j].getcname()))
     for i in range(len(student_list)):
         mark = Mark(student_list[i], course_list[j], "")
         mark.inputmark()
         mark_list += [mark]
+
+    screen.clear()
+    screen.refresh()
 
 
 def calculate_gpa(sname):
@@ -154,46 +159,84 @@ def calculate_gpa(sname):
             student.setgpa(math.floor(total / total_credits))
 
 
-while True:
-    screen.addstr(
-    "\nChoose options you want: \n"
-    "1: Student_info \n"
-    "2: Course info \n"
-    "3: Show mark \n"
-    "4: Calculate gpa \n")
+menu = ['Student', 'Course', 'Mark list', 'GPA']
 
-    screen.refresh()
-    main = int(screen.getstr().decode())
 
-    if main == 1:
-        screen.clear()
-        for student in student_list:
-            student.show_student()
-        screen.refresh()
+def print_menu(stdscr, selected_row_idx):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+    for idx, row in enumerate(menu):
+        x = w // 2 - len(row) // 2
+        y = h // 2 - len(menu) // 2 + idx
+        if idx == selected_row_idx:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y, x, row)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, row)
 
-    elif main == 2:
-        screen.clear()
-        for course in course_list:
-            course.show_course()
-        screen.refresh()
+    stdscr.refresh()
 
-    elif main == 3:
-        screen.clear()
-        for mark in mark_list:
-            mark.show_mark()
-        screen.refresh()
 
-    elif main == 4:
-        screen.clear()
-        for student in student_list:
-            calculate_gpa(student.getsname())
-            screen.addstr(f'{student.getsname()} got  {student.getgpa()} in gpa\n')
-        screen.refresh()
+def main(stdscr):
+    h, w = stdscr.getmaxyx()
 
-    else:
-        break
-    screen.addstr("window ended.")
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    text1 = "Initializing screen...."
+    text2 = "WELCOME"
 
+    x1 = w // 2 - len(text1) // 2
+    y1 = h // 2
+    stdscr.attron(curses.color_pair(1))
+    stdscr.addstr(y1, x1, text1)
+    stdscr.refresh()
+    time.sleep(3)
+    curses.curs_set(0)
+    stdscr.clear()
+
+    x2 = w // 2 - len(text2) // 2
+    y2 = h // 2
+    stdscr.attron(curses.color_pair(2))
+    stdscr.addstr(y2, x2, text2)
+    stdscr.refresh()
+    time.sleep(2.2)
+    curses.curs_set(0)
+    stdscr.clear()
+
+    current_row_idx = 0
+    print_menu(stdscr, current_row_idx)
+    while True:
+        key = stdscr.getch()
+        stdscr.clear()
+        if key == curses.KEY_UP and current_row_idx > 0:
+            current_row_idx -= 1
+        elif key == curses.KEY_DOWN and current_row_idx < len(menu) - 1:
+            current_row_idx += 1
+        elif key == curses.KEY_ENTER or key in [10, 13] and current_row_idx == 0:
+            for student in student_list:
+                student.show_student()
+            stdscr.refresh()
+            stdscr.getch()
+        elif key == curses.KEY_ENTER or key in [10, 13] and current_row_idx == 1:
+            for course in course_list:
+                course.show_course()
+            stdscr.refresh()
+            stdscr.getch()
+        elif key == curses.KEY_ENTER or key in [10, 13] and current_row_idx == 2:
+            for mark in mark_list:
+                mark.show_mark()
+            stdscr.refresh()
+            stdscr.getch()
+        elif key == curses.KEY_ENTER or key in [10, 13] and current_row_idx == 3:
+            for student in student_list:
+                calculate_gpa(student.getsname())
+                stdscr.addstr(f'{student.getsname()} got {student.getgpa()} in gpa\n')
+            stdscr.refresh()
+            stdscr.getch()
+        print_menu(stdscr, current_row_idx)
+        stdscr.refresh()
+curses.wrapper(main)
 
 # print(*map(lambda m: m.getcname(), course_list), sep='\n')
 # course_list[0].getcname() == mark_list[0].getcourse().getcname()
